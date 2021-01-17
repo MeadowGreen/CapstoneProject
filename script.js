8796dc909c18ad7687e93079989c000a51f76a6a
Meadow Green <meadowg.4543@gmail.com>
2021-01-17 14:55:51 -0700

    About page complete

--- script.js
+++ script.js
@@ -194,12 +194,6 @@ function alertFunc() {
     });
   });
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
